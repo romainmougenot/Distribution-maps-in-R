89bb744b5fb4f7382cbaf3560b9ace0908ca3527
Romain Mougenot <100597093+romainmougenot@users.noreply.github.com>
2022-05-06 12:29:48 +0200

    Create Function to aggregate spatial points (observations) into spatia grid.R
    This function is based on a similar function from the Moin library (MOdelling INteraction: https://gitlab.com/CRC1266-A2/moin), but adapted to aggregate observations into a spatial grid.

---
+++ Function to aggregate spatial points (observations) into spatia grid.R
@@ -0,0 +1,25 @@
+## Import libraries
+library(spatstat)
+library(maptools)
+library(sp)
+
+## Function aggr_fea_voro_cell
+# The function takes three arguments in entry: 
+# - grid: a spatial frame with georeferenced cells
+# - win: a spatial frame setting the limits of the spatial frame
+# - features: a data frame with x and y coordinates of observations as columns, and the observation's type as a column (type_col)
+# - type_col: the last column of the features data frame, indicating the types of observations
+# Returns a data frame with 
+
+
+aggr_fea_voro_cell <- function (grid, win, features, type_col) 
+{
+  err <- stats::rnorm(length(features[, 1]), mean = 0, sd = 1e-04)
+  features[, 1] <- features[, 1] + err
+  ppp_nd <- maptools::as.ppp.SpatialPoints(grid)
+  ppp_fea <- ppp_fea <- spatstat.geom::as.ppp(features[, c(1, 2)], W = win)
+  voronoi <- spatstat.geom::dirichlet(ppp_nd)
+  ppp_assign <- spatstat.geom::cut.ppp(ppp_fea, voronoi)
+  df_aggr_fea <- cbind(features[type_col], nodes = as.numeric(ppp_assign[["marks"]]))
+  return(df_aggr_fea)
+}
