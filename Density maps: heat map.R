685783dbf15c6a5a013fd786eabd10ff07c1b5ba
Romain Mougenot <100597093+romainmougenot@users.noreply.github.com>
2022-05-06 11:06:15 +0200

    Update Density maps: heat map.R

--- Density maps: heat map.R
+++ Density maps: heat map.R
@@ -3,38 +3,38 @@ library(sf)
 library(spatstat)
 
 ## Import data
-# Vectorial map.
+# vectorial map
 map <- sf::st_read("nne_10m_coastline.shp") 
-# Inventory data frame with x and y coordinates of points as columns; the two columns shoud be names as x and y respectively
+# inventory data frame with x and y coordinates of points as columns; the two columns shoud be named as x and y respectively
 inventory <- read.csv("inventory.csv", header = TRUE, ";") 
 
-# Bounding box (limits of the spatial box): contains the Nort-West and South-East corners of 
-# the extent of the spatial bow. Data frame with with x min and y min as a column, and x max and y max as a second column.
+# bounding box (limits of the spatial box): contains the Nort-West and South-East corners of 
+# the extent of the spatial box: data frame with with x min and y min as a column, and x max and y max as a second column
 bb <- read.csv("bb3.csv", header = TRUE, ";") 
 
-
 ## Crop map
-# Function from sf library which crops the map according to the x max and y max, and x min and y min, coordinates of the map's subset.
+# function from sf library which crops the map according to the x max and y max, and x min and y min, coordinates of the map's subset
 map_crop <- sf::st_crop(map, xmin = 13.31005, ymin = 43.72676, xmax = 16.21319, ymax = 45.60185) 
 
-
 ## Transform data into spatial data
-# Set bounding box as spatial.
+# set bounding box as spatial.
 coordinates(bb) = ~x+y 
 # Set inventory as spatial.
 coordinates(inventory) = ~x+y 
 
 ## Create a point pattern
-# Create a bounding box for the point pattern.
+# create a bounding box for the point pattern
 bb = bbox(bb) 
 win <- owin(xrange = c(bb[1,1], bb[1,2]), yrange = c(bb[2,1], bb[2,2])) 
 
-# Remove duplicated points to avoid cluping, and create the point pattern.
+# remove duplicated points to avoid cluping
 inventory <- remove.duplicates(inventory, zero = 0, remove.second = TRUE)
+# create the point pattern
 ppp_inventory <- spatstat.geom::ppp(inventory@coords[,1], inventory@coords[,2], window = win)
 
-# Create heat map by converting the point pattern into a density frame, and add the vectorial map.
+# create heat map by converting the point pattern into a density frame
 plot(density(ppp_inventory, bw.diggle), yaxt = "n", main = NULL,
      box = F,
-     col = gray.colors(5, start = 1, end = 0.4))
+     col = gray.colors(5, start = 1, end = 0.4)) # set color palette
+# add the vectorial map
 plot(map_crop$geometry, add = T)
